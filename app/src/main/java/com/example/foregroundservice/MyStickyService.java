9a5afa68e59af597766eb9d1c9602a89f287d714
stojkovmarjan <stojkovmarjan@gmail.com>
2023-08-05 19:50:17 +0200

    Done

--- app/src/main/java/com/example/foregroundservice/MyStickyService.java
+++ app/src/main/java/com/example/foregroundservice/MyStickyService.java
@@ -1,14 +1,52 @@
 package com.example.foregroundservice;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class MyStickyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            Log.e ("Service", "Service is running");
+                            try {
+                                    Thread.sleep(2000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        ).start();
+
+        final String CHANNEL_ID = "My Foreground ID";
+
+        NotificationChannel notificationChannel = new NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_ID,
+            NotificationManager.IMPORTANCE_LOW
+        );
+
+        getSystemService(NotificationManager.class).createNotificationChannel(notificationChannel);
+
+        Notification.Builder notification = new Notification.Builder (this, CHANNEL_ID)
+                .setContentText("Foreground service is running!")
+                .setContentTitle("Service enabled")
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        startForeground(1001, notification.build());
+
         return super.onStartCommand(intent, flags, startId);
     }
 
